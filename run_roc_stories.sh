72da1a3a5662feb14dd4c41c5358937531f26b05
jamesmullenbach <james.mullenbach@gmail.com>
2018-07-10 12:03:08 -0700

    hpc

--- run_roc_stories.sh
+++ run_roc_stories.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /usr/usc/cuda/8.0/setup.sh
 source /usr/usc/cuDNN/v6.0-cuda8.0/setup.sh
-python train.py --dataset rocstories --desc rocstories --submit --analysis --data_dir $1
+srun -n1 python train.py --dataset rocstories --desc rocstories --submit --analysis --data_dir $1
